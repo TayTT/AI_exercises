9a3c3cf607214fe3f0c5978553df2abb9e93c8ab
Tay <marlenapodlesna@gmail.com>
2023-05-19 00:56:14 +0200

    Base algorythm works

--- FrozenLake_v0/main.py
+++ FrozenLake_v0/main.py
@@ -1,29 +1,82 @@
+import copy
 import gym
 import numpy as np
-import random
+import matplotlib.pyplot as plt
 
-environment = gym.make("FrozenLake-v1", is_slippery=False, render_mode="human")
-environment.reset()
-environment.render()
+# Train ======================================================================================
+epochs = 10000
+alpha = 0.1            # Learning rate
+gamma = 0.9            # Discount factor
 
-#QTable: rows -> every state s, cols -> every action a, cells -> quality of the action in the state s
+# environment = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=True, render_mode="human")
+environment = gym.make("FrozenLake-v1", map_name="8x8", is_slippery=True)
 
-# qtable = np.zeros((16, 4))
+# QTable: rows -> every state s, cols -> every action a, cells -> quality of the action in the state s
+qtable = np.zeros((environment.observation_space.n, environment.action_space.n ))
+qtable_init = copy.deepcopy(qtable)
+outcomes = []
 
-# Alternatively, the gym library can also directly g
-# give us the number of states and actions using
-# "env.observation_space.n" and "env.action_space.n"
-nb_states = environment.observation_space.n  # = 16
-nb_actions = environment.action_space.n      # = 4
-qtable = np.zeros((nb_states, nb_actions))
+for i in range(epochs):
+    if i % 25 == 0: print(i)
+    state = environment.reset()[0]
+    terminated = False
 
-# random.choice(["LEFT", "DOWN", "RIGHT", "UP"])
-action = environment.action_space.sample()
 
-# 2. Implement this action and move the agent in the desired direction
-print(environment.step(action))
-new_state, reward, done, info = environment.step(action)
+    while not terminated:
+        # Kolumna - jakość akcji -> wybierz akcję o najwyższej wartości lub losową jeśli 0
+        if np.max(qtable[state]) > 0:
+            action = np.argmax(qtable[state])
+        else:
+            action = environment.action_space.sample()
 
-# Display the results (reward and map)
-environment.render()
-print(f'Reward = {reward}')
+        observation, reward, terminated, truncated, info = environment.step(action)
+
+        qtable[state, action] = qtable[state, action] + \
+                                alpha * (reward + gamma * np.max(qtable[int(observation)]) - qtable[state, action])
+
+        state = observation
+
+        if reward:
+            outcomes.append(1)
+        if terminated and reward == 0.0:
+            outcomes.append(0)
+            break
+
+print('Q-table after training:')
+print(qtable)
+
+plt.plot(outcomes)
+plt.title("Goal reached in every epoch")
+plt.xlabel("Run number")
+plt.ylabel("Outcome")
+plt.show()
+
+# Test ======================================================================================
+test_epochs = 1000
+test_outcomes = []
+success_evaluation = []
+
+for i in range(10):
+    for i in range(test_epochs):
+        state = environment.reset()[0]
+        terminated = False
+
+        while not terminated:
+            action = np.argmax(qtable[state])
+            observation, reward, terminated, truncated, info = environment.step(action)
+            state = observation
+            if reward:
+                test_outcomes.append(1)
+            if terminated and reward == 0.0:
+                test_outcomes.append(0)
+    success_evaluation.append(test_outcomes.count(1))
+    test_outcomes = []
+print(success_evaluation)
+
+print(f"In last iteration there were {test_outcomes.count(1)} successes. Success rate: {test_outcomes.count(1) / test_epochs * 100}%")
+eval_percentage = [i/test_epochs*100 for i in success_evaluation]
+plt.plot(eval_percentage)
+plt.title("Percent of correct paths in each evaluation")
+plt.xlabel("Evaluation number")
+plt.ylabel("Percentage")
+plt.show()
